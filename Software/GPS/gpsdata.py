fef9a84641852dfb609c17b444a6cd18b8ec8467
Timothy Warren <timlwarren@gmail.com>
2022-07-22 17:29:56 -0700

    New stuff

--- Software/GPS/gpsdata.py
+++ Software/GPS/gpsdata.py
@@ -7,7 +7,10 @@ This script is meant to continuosuly write to a text file..
 
 how accurate the GPS coordinates are with the current module over time
 
-Last Editted:  7/16/2022 by Logan Rower
+Last Editted:  7/22/2022 by Logan Rower
+- For Pi1 but can be editted for other pis such that file and folder names
+are labeled for the experiment starttime and with the specific pi number the data
+is from
 """
 
 # Simple GPS module demonstration.
@@ -65,19 +68,28 @@ gps.send_command(b"PMTK220,5000")
 time.sleep(5)
 # First set up the folder that will essentially be responsible for holding the data for
 ## The Day in which experiments took place. Then csv files will be saved based on the start time of the experiment
-path = "/home/pi/Desktop/Field_Trap/GPS_data/"
-start_time = datetime.now() # will use start time when we are in the end making the csv file for the experiment...
-print(start_time)
+path = "/home/pi/Field_Trap/GPS_data/"
+
+# load the json file in
+with open('control.json') as json_file:
+    control_data = json.load(json_file)
+start_time = control_data['start_time']
+duration = control_data['duration']
+
+# set the duration (SET IN JSON)
+time_hr = duration[0]
+time_min = duration[1]
+time_sec = duration[2]
 
 # Set the time to end the experiment
 ## Experiment runs for 5 minutes
-tdelta =  timedelta(minutes = 5)
+tdelta = timedelta(seconds = time_sec, minutes = time_min, hours = time_hr)
 end_time = start_time +tdelta
 
 print(f"End time:{end_time}")
 
 # created the folder for the start date
-gps_fold_name = str(start_time.strftime("%Y-%m-%d"))
+gps_fold_name ="Pi1_"+start_time.strftime("%Y%m%d%H%M%S")
 path_new = os.path.join(path,gps_fold_name )
 os.makedirs(path_new, exist_ok = True)
 
@@ -88,28 +100,29 @@ last_print = int(start_time.strftime("%Y%m%d%H%M%S"))
 # # that will be used to house both the time series data as well as the lat and long data
 # recorded time in seconds (will run experiment for 5 minutes...)
 
-location = path_new + "/%s.txt"
+location = path_new + "/Pi1_%s.txt"
 filepath = location % start_time.strftime("%Y%m%d%H%M%S")
-with open(filepath, 'w') as writer:
-    while True:
-        # Make sure to call gps.update() every loop iteration and at least twice
-        # as fast as data comes from the GPS unit (usually every second).
-        # This returns a bool that's true if it parsed new data (you can ignore it
-        # though if you don't care and instead look at the has_fix property).
-        time.sleep(5)
-        gps.update()
-        # Every second print out current location details if there's a fix.
-        # Also will save to a new file in the directory GPS_DATA
-        current = datetime.now()
-        diff = (current - start_time)
-        diff_sec =int(diff.total_seconds())
-        # gps long and lat
-        lat = gps.latitude
-        lon = gps.longitude
+if current_time >= start_time:
+    with open(filepath, 'w') as writer:
+        while True:
+            # Make sure to call gps.update() every loop iteration and at least twice
+            # as fast as data comes from the GPS unit (usually every second).
+            # This returns a bool that's true if it parsed new data (you can ignore it
+            # though if you don't care and instead look at the has_fix property).
+            time.sleep(5)
+            gps.update()
+            # Every second print out current location details if there's a fix.
+            # Also will save to a new file in the directory GPS_DATA
+            current = datetime.now()
+            diff = (current - start_time)
+            diff_sec =int(diff.total_seconds())
+            # gps long and lat
+            lat = gps.latitude
+            lon = gps.longitude
 
-        # saves data to the text file to the folder..
-        writer.write(str(diff_sec)+" "+str(lat)+" "+str(lon)+"\n")
-        # continues through while loop until end time reached
-        print(datetime.now())
-        if datetime.now() >= end_time:
-            break
+            # saves data to the text file to the folder..
+            writer.write(str(diff_sec)+" "+str(lat)+" "+str(lon)+"\n")
+            # continues through while loop until end time reached
+            print(datetime.now())
+            if datetime.now() >= end_time:
+                break
