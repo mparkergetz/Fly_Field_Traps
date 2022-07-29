8ed02693a06590b95bd0b869a821014ccc1006d8
lkrower <lkrower2000@gmail.com>
2022-07-28 22:23:07 -0700

    checks the files

--- Software/Data_Extraction/framecheck.py
+++ Software/Data_Extraction/framecheck.py
@@ -9,14 +9,15 @@ date = input("Input the Date of Release (YYYY-MM-DD")
 pi_num = input("Input the Pi Number: ")
 release_time = input("Release Precise Time (YYYYMMDDHHMMSS)")
 timelapse_fold = pi_num+'_'+release_time
-path = '/media/flyranch/Samsung_T5/Field_Trap_Exps/'+date+'/'+timelapse_fold
-
+#path = '/media/flyranch/Samsung_T5/Field_Trap_Exps/'+date+'/'+timelapse_fold
+path_log = '/Desktop/Pi1_20220728141153'
 # change directory to this directory
-os.chdir(path)
-
+os.chdir(path_log)
+dir_list=os.listdir(path_log)
+print(dir_list)
 # now within the timelapse directory we need to check to see if all files minus the first are in increments of
-# the framerate... in this case 2 seconds.
+# the framerate..d. in this case 2 seconds.
 
-https://www.geeksforgeeks.org/python-list-files-in-a-directory/
+#https://www.geeksforgeeks.org/python-list-files-in-a-directory/
 
 
